85c5914a9e04cc398543b066370a7602aaa49a20
bataaatomica <129532053+bataaatomica@users.noreply.github.com>
2025-03-16 18:55:02 -0300

    Create AumentoSalario.java

---
+++ AumentoSalario.java
@@ -0,0 +1,17 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class AumentoSalario {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite seu salário atual: ");
+        double salarioAtual = scanner.nextDouble();
+        double novoSalario = salarioAtual * 1.25;
+
+        System.out.println("O seu novo salário é: " + novoSalario);
+
+        scanner.close();
+    }
+
+}
